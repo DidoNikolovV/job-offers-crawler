33c2f1d6f013c92ed02da44705c875be45982860
Diyan Nikolov <didonikolov01@gmail.com>
2025-01-26 22:48:45 +0200

    Initial commit

---
+++ src/main/java/com/olimpiads/webcrawlerapp/WebcrawlerAppApplication.java
@@ -0,0 +1,13 @@
+package com.olimpiads.webcrawlerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcrawlerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcrawlerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olimpiads/webcrawlerapp/WebcrawlerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olimpiads.webcrawlerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
